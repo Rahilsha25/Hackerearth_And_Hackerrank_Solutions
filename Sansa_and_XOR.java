2dcff4ffab39ff099fbe3c1e63ba76455abd5aa3
RAHIL SHA <48413829+Rahilsha25@users.noreply.github.com>
2020-07-31 09:38:53 +0530

    Update Sansa_and_XOR.java

--- Sansa_and_XOR.java
+++ Sansa_and_XOR.java
@@ -1,6 +1,14 @@
-import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-static int sansaXor(int[] array) {
+public class Solution {
+
+   static int sansaXor(int[] array) {
     if (array.length % 2 == 0) { // Case 1
         return 0;
     } else { // Case 2
@@ -11,5 +19,38 @@ static int sansaXor(int[] array) {
             }
         }
        return result;
+    }
+}
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] arr = new int[n];
+
+            String[] arrItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr[i] = arrItem;
+            }
+
+            int result = sansaXor(arr);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
