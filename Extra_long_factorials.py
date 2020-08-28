d6bfb6b7e038cd8afb0abb039d26a5750abf52a5
RAHIL SHA <48413829+Rahilsha25@users.noreply.github.com>
2020-08-28 15:53:11 +0530

    Create Extra_long_factorials.py

---
+++ Extra_long_factorials.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    ans=1
+    while(n!=1):
+        ans*=n
+        n-=1
+    print (ans)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    extraLongFactorials(n)
